7ff1329487619f07dade070c14f18f360a68260e
Matthew Becker <becker@tamu.edu>
2021-05-16 15:37:46 -0500

    Create memorymap.h

---
+++ FSW/container/memorymap.h
@@ -0,0 +1,13 @@
+// Container memory map, each value is an eeprom addr
+byte ADDR_software_state = 		0;
+byte ADDR_time_ss = 			1;
+byte ADDR_time_mm = 			2;
+byte ADDR_time_hh = 			3;
+byte ADDR_packet_count = 		4;
+byte ADDR_mode = 				24;
+byte ADDR_sim_enabled = 		25;
+byte ADDR_sim_pressure = 		26;
+byte ADDR_sp1_released = 		30;
+byte ADDR_sp2_released = 		31;
+byte ADDR_sp1_packet_count = 	32;
+byte ADDR_sp2_packet_count = 	36;
