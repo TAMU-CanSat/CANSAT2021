cc77d8c90498933d7c60183ef8d361dcfe2adb85
Matthew Becker <becker@tamu.edu>
2021-05-16 17:01:59 -0500

    Update memorymap with RTC_old

--- FSW/container/memorymap.h
+++ FSW/container/memorymap.h
@@ -1,13 +1,27 @@
+// Used to offset EEPROM addresses to not overstress individual bytes
+short EEPROM_OFFSET = 0;  
+
 // Container memory map, each value is an eeprom addr
-byte ADDR_software_state = 		0;
-byte ADDR_time_ss = 			1;
-byte ADDR_time_mm = 			2;
-byte ADDR_time_hh = 			3;
-byte ADDR_packet_count = 		4;
-byte ADDR_mode = 				24;
-byte ADDR_sim_enabled = 		25;
-byte ADDR_sim_pressure = 		26;
-byte ADDR_sp1_released = 		30;
-byte ADDR_sp2_released = 		31;
-byte ADDR_sp1_packet_count = 	32;
-byte ADDR_sp2_packet_count = 	36;
+short ADDR_software_state = 		0  + EEPROM_OFFSET;
+short ADDR_time_ss = 			1  + EEPROM_OFFSET;
+short ADDR_time_mm = 			2  + EEPROM_OFFSET;
+short ADDR_time_hh = 			3  + EEPROM_OFFSET;
+short ADDR_packet_count = 		4  + EEPROM_OFFSET;
+short ADDR_mission_time_ss = 	8  + EEPROM_OFFSET;
+short ADDR_mission_time_mm = 	9  + EEPROM_OFFSET;
+short ADDR_mission_time_hh = 	10 + EEPROM_OFFSET;
+short ADDR_mode = 				24 + EEPROM_OFFSET;
+short ADDR_sim_enabled = 		25 + EEPROM_OFFSET;
+short ADDR_sim_pressure = 		26 + EEPROM_OFFSET;
+short ADDR_sp1_released = 		30 + EEPROM_OFFSET;
+short ADDR_sp2_released = 		31 + EEPROM_OFFSET;
+short ADDR_sp1_packet_count = 	32 + EEPROM_OFFSET;
+short ADDR_sp2_packet_count = 	36 + EEPROM_OFFSET;
+
+// State aliases
+byte LAUNCH_WAIT = 1;
+byte ASCENT_LAUNCHPAD = 2;
+byte DESCENT = 3;
+byte SP1_RELEASE = 4;
+byte SP2_RELEASE = 5;
+byte LANDED = 6;
