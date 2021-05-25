ee3b15fc20650e8870169fccd23cf7229197c9a7
Matthew Becker <becker@tamu.edu>
2021-05-25 18:13:27 -0500

    Finish cleanup sketch

--- FSW/container_cleanup/container_cleanup.ino
+++ FSW/container_cleanup/container_cleanup.ino
@@ -1,11 +1,23 @@
 #include <EEPROM.h>
 
-void setup() {
-  // TODO Overwrite EEPROM addresses
+// COPY FROM MEMORYMAP.h IN CONTAINER
+const short EEPROM_OFFSET = 0;  
 
+// Container memory map, each value is an eeprom addr
+const short ADDR_software_state =     0  + EEPROM_OFFSET;
+// END COPIED REGION
+
+
+void setup() {
+  // Default EEPROM state
+  EEPROM.update(ADDR_software_state, 1);
 }
 
+int ledPin = 13;
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(100000);
+  // Blink to show completion
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
 }
